1175f8d9b5307f78bd2dccfdaa13863deeb515a6
kagemeka <kagemeka1@gmail.com>
2021-07-10 18:55:51 +0900

    enhance: create dart stage (#33)

---
+++ env/dart/setup.sh
@@ -0,0 +1,17 @@
+apt -q=2 update \
+&& apt install -yq=2 \
+apt-utils \
+apt-transport-https \
+gnupg2 \
+wget \
+&& wget -qO - \
+https://dl-ssl.google.com/linux/linux_signing_key.pub \
+| apt-key add - \
+&& wget -qO - \
+https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list \
+> /etc/apt/sources.list.d/dart_stable.list \
+&& apt -q=2 update \
+&& apt -yq=2 install dart \
+&& echo \
+'export PATH="$PATH:/usr/lib/dart/bin"' \
+>> ~/.bashrc
