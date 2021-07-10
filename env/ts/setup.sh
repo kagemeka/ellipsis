c31bc1e4f08f8d702cc9a2760e533865401aa922
kagemeka <kagemeka1@gmail.com>
2021-07-11 01:26:05 +0900

    enhance: create typescript stage (#41)

---
+++ env/ts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update -q=2 \
+&& apt install -yq=2 \
+apt-utils \
+curl \
+sudo \
+&& curl \
+-sL https://deb.nodesource.com/setup_current.x \
+| sudo -E bash - \
+&& apt install -y nodejs \
+&& npm install -g \
+typescript \
+ts-node
